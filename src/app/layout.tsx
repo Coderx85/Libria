f63fcd2221a058d7536cca956c01ef98b7e87241
Coderx85 <work.priyanshu085@gmail.com>
2025-04-15 23:09:10 +0530

    feat: Refactor project structure and add global styles, layout, and configuration

---
+++ src/app/layout.tsx
@@ -0,0 +1,52 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Toaster } from "sonner";
+
+import localFont from "next/font/local";
+import { ReactNode } from "react";
+// import { SessionProvider } from "next-auth/react";
+// import { auth } from "@/auth";
+
+const ibmPlexSans = localFont({
+  src: [
+    { path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
+  ],
+});
+
+const bebasNeue = localFont({
+  src: [
+    { path: "/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
+  ],
+  variable: "--bebas-neue",
+});
+
+export const metadata: Metadata = {
+  title: "Libria",
+  description:
+    "Libria is a web application that allows you to manage your books and reading lists. It provides a simple and intuitive interface for tracking your reading progress, organizing your books, and discovering new titles.",
+};
+
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  // const session = await auth();
+
+  return (
+    <html lang="en">
+      {/* <SessionProvider 
+      session={session}
+      > */}
+        <body
+          className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
+        >
+          {children}
+
+          <Toaster />
+        </body>
+      {/* </SessionProvider> */}
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ types.d.ts
@@ -0,0 +1,41 @@
+interface Book {
+  id: string | number; // Change to only string
+  title: string;
+  author: string;
+  genre: string;
+  rating: number;
+  totalCopies: number;
+  availableCopies: number;
+  description: string;
+  coverColor: string;
+  coverUrl: string;
+  videoUrl: string;
+  summary: string;
+  createdAt?: Date | null; // Date when the book was added to the database
+}
+
+interface AuthCredentials {
+  fullName: string;
+  email: string;
+  password: string;
+  universityId: number;
+  universityCard: string;
+}
+
+interface BookParams {
+  title: string;
+  author: string;
+  genre: string;
+  rating: number;
+  coverUrl: string;
+  coverColor: string;
+  description: string;
+  totalCopies: number;
+  videoUrl: string;
+  summary: string;
+}
+
+interface BorrowBookParams {
+  bookId: string;
+  userId: string;
+}
