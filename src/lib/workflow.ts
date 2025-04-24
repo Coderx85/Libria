e83d6104f2054b856b90763452d8743263b9661c
Coderx85 <work.priyanshu085@gmail.com>
2025-04-24 23:41:06 +0530

    feat: Add onboarding workflow and email notification system

---
+++ src/lib/workflow.ts
@@ -0,0 +1,24 @@
+import { Client as WorkflowClient } from "@upstash/workflow";
+import { Client as QStashClient } from "@upstash/qstash";
+import config from "@/lib/config";
+
+export const workflowClient = new WorkflowClient({
+  baseUrl: config.env.upstash.qstashUrl,
+  token: config.env.upstash.qstashToken,
+});
+
+const qstashClient = new QStashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  // Do it Later.
+};
